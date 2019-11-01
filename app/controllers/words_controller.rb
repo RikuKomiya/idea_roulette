9b85ef9a3e172803f841ae66039757654f10b525
RikuKomiya <824rikukomiya@gmail.com>
2019-11-01 04:48:24 +0000

    finish word-crud

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,10 +1,46 @@
 class WordsController < ApplicationController
+  before_action :set_word, only: [:edit,:update,:destroy]
+
+  def index
+    @words = Word.all
+  end
+
   def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to new_word_path, notice: "「#{@word.name}」を登録しました"
+    else
+      render :new
+    end
   end
 
   def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @word.destroy
+    redirect_to words_path, notice: "「#{@word.name}」を削除しました"
   end
 
   def confirm_new
+  end
+
+
+  private
+  def word_params
+    params.require(:word).permit(:name)
+  end
+
+  def set_word
+    @word = Word.find(params[:id])
   end
 end
